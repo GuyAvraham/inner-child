455c37b318bedf80758ce19ba809c2e6900f56d0
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-07-19 10:33:12 +0300

    🔨 refactor(api): changes logic to storing only s3 key in db

--- packages/api/src/router/picture.ts
+++ packages/api/src/router/picture.ts
@@ -1,45 +1,74 @@
+import { TRPCError } from "@trpc/server";
+import { map } from "radash";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const pictureRouter = createTRPCRouter({
-  createS3UploadLink: protectedProcedure
-    .input(
-      z.object({
-        fileName: z.string(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      const fileName = `picture-${Date.now()}.${input.fileName
-        .split(".")
-        .at(-1)}`;
-      const urlToUpload = await ctx.s3.createPresignedUrl(fileName);
-
-      const urlToSave = await ctx.s3.getUrlForDb(fileName);
-
-      return { urlToUpload, urlToSave };
-    }),
-  saveMetadata: protectedProcedure
+  getUploadURI: protectedProcedure
     .input(
       z.object({
         age: z.enum(["OLD", "YOUNG", "CURRENT"]),
-        s3URI: z.string().url(),
+        ext: z.string(),
       }),
     )
-    .mutation(({ ctx, input: { age, s3URI } }) => {
-      return ctx.prisma.picture.create({
+    .mutation(async ({ ctx, input }) => {
+      const fileName = `${ctx.session.userId}-${
+        crypto.randomUUID().split("-")[0]
+      }.${input.ext}`;
+
+      // TODO: add conversion to one format
+
+      const s3Key = `${input.age}/${fileName}`;
+
+      const urlToUpload = await ctx.s3.createPresignedUrl(s3Key);
+
+      await ctx.prisma.picture.create({
         data: {
-          age,
-          uri: s3URI,
+          age: input.age,
+          key: s3Key,
           userId: ctx.session.userId,
+        },
+      });
+
+      return urlToUpload;
+    }),
+  getURI: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const picture = await ctx.prisma.picture.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!picture) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return ctx.s3.getPresignedUrl(picture.key);
+    }),
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.picture.findUnique({
+        where: {
+          id: input.id,
         },
       });
     }),
   getAll: protectedProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.picture.findMany({
+    const pictures = await ctx.prisma.picture.findMany({
       where: {
         userId: ctx.session.userId,
       },
+    });
+
+    return map(pictures, async (picture) => {
+      const uri = await ctx.s3.getPresignedUrl(picture.key);
+
+      return {
+        ...picture,
+        uri,
+      };
     });
   }),
 });
