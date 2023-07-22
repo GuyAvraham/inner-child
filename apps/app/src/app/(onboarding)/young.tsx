c7a9101c14ac6823b6a819b2fb22ea08952f6901
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-07-22 13:02:24 +0300

    ✨ feat(app): adds global cross-screen store

--- apps/app/src/app/(onboarding)/young.tsx
+++ apps/app/src/app/(onboarding)/young.tsx
@@ -1,24 +1,28 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Button, Text } from "react-native";
 import type { ImagePickerAsset } from "expo-image-picker";
 import { useRouter } from "expo-router";
 import { ClerkLoaded, ClerkLoading, useUser } from "@clerk/clerk-expo";
+import { useAtom, useAtomValue } from "jotai";
 
 import SubmitPhoto from "~/components/SelectPhoto";
 import { ROUTE } from "~/config/routes";
 import { useSubmitPhoto } from "~/hooks/useSavePhoto";
+import { photoAtom, youngPhotoAtom } from "~/store/photos";
 
 export default function YoungPhotoScreen() {
-  const [photo, setPhoto] = useState<ImagePickerAsset | undefined>();
   const router = useRouter();
   const { user } = useUser();
+
+  const photo = useAtomValue(photoAtom);
+  const [youngPhoto, setYoungPhoto] = useAtom(youngPhotoAtom);
 
   const { isSubmitting, submitPhoto } = useSubmitPhoto();
 
   const handleSubmit = useCallback(async () => {
-    if (!photo) return;
+    if (!youngPhoto) return;
 
-    await submitPhoto(photo, "YOUNG");
+    await submitPhoto(youngPhoto, "YOUNG");
 
     await user?.update({
       unsafeMetadata: {
@@ -27,7 +31,7 @@ export default function YoungPhotoScreen() {
     });
 
     router.push(ROUTE.HOME.MAIN);
-  }, [photo, router, submitPhoto, user]);
+  }, [router, submitPhoto, user, youngPhoto]);
 
   return (
     <>
@@ -36,7 +40,7 @@ export default function YoungPhotoScreen() {
       </ClerkLoading>
       <ClerkLoaded>
         <Text>Upload a photo of you as a child</Text>
-        <SubmitPhoto onSelect={setPhoto} />
+        <SubmitPhoto onSelect={setYoungPhoto} />
         <Button
           title="generate automatically"
           onPress={async () => {
