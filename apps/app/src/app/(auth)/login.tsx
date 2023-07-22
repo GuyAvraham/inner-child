4b57161d9e12d744780e24747fdaf4c73e8ee064
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-07-22 13:47:07 +0300

    🧹 chore(app): improves styling of login screen

--- apps/app/src/app/(auth)/login.tsx
+++ apps/app/src/app/(auth)/login.tsx
@@ -1,12 +1,18 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 import LogInButton from "~/components/LogInButton";
 
-export default function Login() {
+export default function LoginScreen() {
   return (
-    <>
-      <Text>Log in</Text>
-      <LogInButton />
-    </>
+    <View className="flex h-full p-8">
+      <View className="flex flex-1 justify-center">
+        <Text className="text-lg">
+          Some welcome image and text. Maybe even carousel 😏
+        </Text>
+      </View>
+      <View className="flex h-1/4 items-center justify-center">
+        <LogInButton />
+      </View>
+    </View>
   );
 }
