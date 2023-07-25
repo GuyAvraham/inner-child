6449fc57bed42f287a7ebb24e9c391d6de2a3a69
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-07-25 19:36:40 +0300

    ✨ feat(lib): adds raise, sleep and blobToDataURL util funcs

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,1 +1,18 @@
-export * from "./src/transformer";
+export { transformer } from "./src/transformer";
+
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export const raise = (error: string): never => {
+  throw new Error(error);
+};
+
+export const blobToDataURL = async (blob: Blob) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = () => reject(reader.error);
+    reader.onabort = () => reject(new Error("Read aborted"));
+    reader.readAsDataURL(blob);
+  });
+};
