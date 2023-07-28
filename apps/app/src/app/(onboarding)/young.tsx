4adfcb52ef42bad427fa137f2866a096180da4b4
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-07-28 12:28:31 +0300

    🔨 refactor(app): removes onboarding flag after submit young photo

--- apps/app/src/app/(onboarding)/young.tsx
+++ apps/app/src/app/(onboarding)/young.tsx
@@ -1,7 +1,6 @@
 import { useCallback } from "react";
 import { Button, Text } from "react-native";
 import { useRouter } from "expo-router";
-import { ClerkLoaded, ClerkLoading, useUser } from "@clerk/clerk-expo";
 import { useAtom } from "jotai";
 
 import SelectionPhoto from "~/components/SelectionPhoto";
@@ -13,7 +12,6 @@ import { AgeMode } from "~/types";
 
 export default function YoungPhotoScreen() {
   const router = useRouter();
-  const { user } = useUser();
 
   const [youngPhoto, setYoungPhoto] = useAtom(youngPhotoAtom);
 
@@ -24,39 +22,28 @@ export default function YoungPhotoScreen() {
 
     await uploadPhoto(youngPhoto, AgeMode.YOUNG);
 
-    await user?.update({
-      unsafeMetadata: {
-        onboarded: true,
-      },
-    });
-
-    router.push(ROUTE.HOME.MAIN);
-  }, [youngPhoto, uploadPhoto, user, router]);
+    router.push(ROUTE.ONBOARDING.GENERATING);
+  }, [youngPhoto, uploadPhoto, router]);
 
   return (
     <>
-      <ClerkLoading>
-        <Text>Loading...</Text>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <Text>Upload a photo of you as a child</Text>
-        <SelectionPhoto photo={youngPhoto} />
-        <SubmitPhoto onSelect={setYoungPhoto} />
-        <Button
-          title="generate automatically"
-          onPress={() => {
-            router.push({
-              pathname: ROUTE.ONBOARDING.GENERATING,
-              params: { young: true },
-            });
-          }}
-        />
-        <Button
-          title={isUploading ? "submitting..." : "submit"}
-          onPress={handleSubmit}
-          disabled={!youngPhoto}
-        />
-      </ClerkLoaded>
+      <Text>Upload a photo of you as a child</Text>
+      <SelectionPhoto photo={youngPhoto} />
+      <SubmitPhoto onSelect={setYoungPhoto} />
+      <Button
+        title="generate automatically"
+        onPress={() => {
+          router.push({
+            pathname: ROUTE.ONBOARDING.GENERATING,
+            params: { young: true },
+          });
+        }}
+      />
+      <Button
+        title={isUploading ? "submitting..." : "submit"}
+        onPress={handleSubmit}
+        disabled={!youngPhoto}
+      />
     </>
   );
 }
