1ceca7a51ed06196d17b5b47d90050a1523cb48c
Yehor Tkach <yehor@guy-avraham.com>
2023-10-23 17:27:40 +0300

    replaced contentful lib to api

--- packages/api/src/contentful.ts
+++ packages/api/src/contentful.ts
@@ -1,16 +1,19 @@
-import { createClient } from 'contentful';
-
 import { defaultPrompts } from './prompts';
 
 const space = process.env.CONTENTFUL_SPACE_ID ?? '';
 const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN ?? '';
 
-const client = createClient({ space, accessToken });
-
 export const contentful = {
   getPrompts: async () => {
-    const entries = await client.getEntries({ locale: 'en-US' }).catch(console.error);
-    const fields = entries?.items[0]?.fields as typeof defaultPrompts | undefined;
+    const response = await fetch(`https://cdn.contentful.com/spaces/${space}/entries`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const json = await response.json();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const fields = json?.items[0]?.fields as typeof defaultPrompts | undefined;
 
     if (!fields) return defaultPrompts;
 
