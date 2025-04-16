481933f7d8bb573e200939e6f13198b048d947c1
ValentineGuyAvrahamCom <valiksegeda@gmail.com>
2025-04-17 00:28:41 +0300

    update gender

--- apps/server/src/components/screens/home/gender.tsx
+++ apps/server/src/components/screens/home/gender.tsx
@@ -5,6 +5,7 @@ import { useUser } from '@clerk/nextjs';
 
 import { generateToken } from '~/utils/token';
 import Button from '~/components/Button';
+import Spinner from '~/components/Spinner';
 import { useRouteState } from '~/hooks/useRouteState';
 
 export default function GenderForm() {
@@ -12,7 +13,7 @@ export default function GenderForm() {
   const { openUpload } = useRouteState();
 
   const setUserGender = useCallback(
-    async (gender: 'male' | 'female') => {
+    (gender: 'male' | 'female') => async () => {
       if (!user) return;
 
       await user.update({ unsafeMetadata: { gender, token: generateToken() } });
@@ -24,18 +25,23 @@ export default function GenderForm() {
 
   return (
     <div className="flex flex-1 flex-col justify-evenly p-4">
-      <p className="my-auto text-center font-[Poppins-Bold] text-4xl leading-[48px] sm:my-0 whitespace-pre-line">
-        We respect all gender identities.{'\n'}For our AI to generate an older image, please select{'\n'} ‘Male’ or ‘Female’—whichever best fits your older self.
+      <p className="my-auto whitespace-pre-line text-center font-[Poppins-Bold] text-4xl leading-[48px] sm:my-0">
+        We respect all gender identities.{'\n'}For our AI to generate an older image, please select{'\n'} ‘Male’ or
+        ‘Female’—whichever best fits your older self.
       </p>
 
-      <div className="mt-4 flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-16">
-        <Button className="w-full min-w-[140px] sm:w-auto" onClick={() => setUserGender('male')}>
-          Male
-        </Button>
-        <Button className="w-full min-w-[140px] sm:w-auto" onClick={() => setUserGender('female')}>
-          Female
-        </Button>
-      </div>
+      {user ? (
+        <div className="mt-4 flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-16">
+          <Button className="w-full min-w-[140px] sm:w-auto" onClick={setUserGender('male')}>
+            Male
+          </Button>
+          <Button className="w-full min-w-[140px] sm:w-auto" onClick={setUserGender('female')}>
+            Female
+          </Button>
+        </div>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
