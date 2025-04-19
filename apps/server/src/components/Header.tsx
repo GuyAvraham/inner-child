077a20370bc97512985fe0b58bfb7f8f095093fe
ValentineGuyAvrahamCom <valiksegeda@gmail.com>
2025-04-19 10:40:04 +0300

    Update Header.tsx

--- apps/server/src/components/Header.tsx
+++ apps/server/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs';
 
 export default function Header() {
   return (
-    <header className="flex min-h-[64px] justify-end p-4">
+    <header className="absolute left-0 top-0 z-30 flex min-h-[64px] justify-end p-4">
       <SignedIn>
         <UserButton afterSignOutUrl="/" />
       </SignedIn>
